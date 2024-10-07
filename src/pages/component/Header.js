22c500e4badd27a6e4dde07a77d8590585b73a1e
infouse <hongyeong575@naver.com>
2024-10-07 16:34:29 +0900

    lhy 10-07 16:34 push commit

---
+++ src/pages/component/Header.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <Head>
+    </Head>
+    <div>
+      <Link></Link>
+    </div>
+    </>
+  )
+}
+
+export default Header
