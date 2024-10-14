6a282945c9c4f4d8930586db78a433433d31b0ca
infouse <hongyeong575@naver.com>
2024-10-14 14:16:15 +0900

    lhy merge2

--- src/pages/component/ReadBtn.js
+++ src/pages/component/ReadBtn.js
@@ -1,17 +1,28 @@
+import { db } from '@/lib/firebase';
 import { collection, getDocs, query, setDoc, where } from 'firebase/firestore'
+import { useSession } from 'next-auth/react';
 import React, { useState } from 'react'
 
 
 const ReadBtn = () => {
   const [readingBtn,setReadBtn] = useState('');
-
-  const docRef = collection
-
-  return (
-    <div>
+  const {data : session} = useSession();
+  const docRef = collection(db, 'readlist');
+  const authhorize = async () => {
+    const q = query(
+      collection(db, "readlist"),
+      where("info.email", "==", session.user.email),
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.docs.map((doc) => {
+      const docRef = doc
+    });
+  }
+    return (
+      <div>
       <button>읽는중</button>
+        
     </div>
   )
 }
-
 export default ReadBtn
